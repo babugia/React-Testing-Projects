6da43d87b0a457001d1880dfcb0773945d18de0c
Guilherme Babugia <vntguba@lw-445-7160.venturus.local>
2019-10-04 07:41:25 -0300

    Basic click counter finished

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
   render() {
     return (
       <div data-test='component-app'>
-        <h1>App</h1>
+        <h1 data-test='counter-display'>
+          The counter is currently {this.state.counter}
+        </h1>
+        <button
+          data-test='increment-button'
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Increment
+        </button>
       </div>
     );
   }
 }
+// TODO: Decrement Button
+// TODO: No count below zero, display an error message saying the counter can't go below zero
+
+//https://github.com/flyrightsister/udemy-react-testing-projects/tree/click-counter-challenges/click-counter
 
 export default App;
